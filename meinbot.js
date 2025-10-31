f4e408ebb7a2581e8944a031b30a7e40f2f8dd6e
Konstanze Mehmedovski <116554947+0815-xyz@users.noreply.github.com>
2025-10-31 10:43:19 +0100

    Initialize chatbot with Flowise configuration

---
+++ meinbot.js
@@ -0,0 +1,5 @@
+import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+Chatbot.init({
+  chatflowid: "6937ddea-a827-4ce1-83d6-a64ccc773baf",  // meine Flowise-Bot-ID
+  apiHost: "https://moodle4.knight.hft-stuttgart.de/flowise-test",
+})
