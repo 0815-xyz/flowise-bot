ab005a5dc13ec67b42f5bd06bf0b43fd5fb85324
Konstanze Mehmedovski <116554947+0815-xyz@users.noreply.github.com>
2025-10-31 11:49:07 +0100

    Update Chatbot ID comment in meinbot.js

--- meinbot.js
+++ meinbot.js
@@ -1,5 +1,5 @@
 import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
 Chatbot.init({
-  chatflowid: "019717c2-3cd4-424f-b449-1fb99e49cc6d",  // meine Flowise-Bot-ID
+  chatflowid: "019717c2-3cd4-424f-b449-1fb99e49cc6d",  // ID von KKM-ChatFlow
   apiHost: "https://moodle4.knight.hft-stuttgart.de/flowise-test",
 })
