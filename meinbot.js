0f74df0cb1bcea0a867ed5d375b6f1b2f3ea4737
Konstanze Mehmedovski <116554947+0815-xyz@users.noreply.github.com>
2025-11-04 11:39:10 +0100

    Delete meinbot.js

--- meinbot.js
+++
@@ -1,5 +0,0 @@
-import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
-Chatbot.init({
-  chatflowid: "019717c2-3cd4-424f-b449-1fb99e49cc6d",  // ID von KKM-ChatFlow
-  apiHost: "https://moodle4.knight.hft-stuttgart.de/flowise-test",
-})
